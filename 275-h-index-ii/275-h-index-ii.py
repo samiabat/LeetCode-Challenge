bb4b5f5ef114f7da9bc192db1ab1ef84b3dcecfd
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-03-17 10:14:21 +0300

    Time: 186 ms (57.76%), Space: 21.1 MB (8.50%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -6,6 +6,7 @@ class Solution:
         while l<=r:
             m = (l+r)//2
             n = size-m
+            print("m",m,"n",n)
             if citations[m]>=n and (m-1<0 or citations[m-1]<n+1):
                 return n
             elif citations[m]>=n:
