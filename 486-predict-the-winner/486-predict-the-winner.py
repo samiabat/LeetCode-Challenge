5534fcf4c8c2ae13d228fe76ef3b0330b41c9137
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-04-22 15:17:53 +0300

    Time: 45 ms (67.12%), Space: 14.1 MB (31.36%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,15 +1,15 @@
 class Solution:
 	def PredictTheWinner(self, nums: List[int]) -> bool:
-		def solve(i, j, chance):
-
+		@lru_cache(None)
+		def dp(i, j, turn):
 			if i > j:
 				return 0
-			if chance == 0:
-				return max(nums[i] + solve(i+1,j,1), nums[j] + solve(i,j-1,1))
+			if turn == 0:
+				return max(nums[i] + dp(i+1,j,1), nums[j] + dp(i,j-1,1))
 			else:
-				return min(solve(i+1,j,0), solve(i, j-1, 0))
+				return min(dp(i+1,j,0), dp(i, j-1, 0))
 
-		two = sum(nums)
-		one = solve(0, len(nums)-1, 0)
-		two -= one
-		return one >= two
+		tot = sum(nums)
+		p1 = dp(0, len(nums)-1, 0)
+		p2 = tot-p1
+		return p1>=p2
