1297fb7a78d7301d9f77ab1dc44d1c9016a2b35a
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-04-29 18:56:13 +0300

    Time: 316 ms (22.52%), Space: 31.5 MB (71.35%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,61 +1,38 @@
-class TrieNode:
-    def __init__(self, char = ""):
-        self.char = char
+class TriNode:
+    def __init__(self, char=""):
         self.children = {}
-        self.is_end = False
-        # self.counter = 0
-
-
+        self.endWith = False
 class Trie:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.root = TrieNode()
-
+        self.root = TriNode()
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         node = self.root
         for char in word:
             if char in node.children:
                 node = node.children[char]
             else:
-                new_node = TrieNode(char)
+                new_node = TriNode(char)
                 node.children[char] = new_node
                 node = new_node
-        node.is_end = True
-        # node.counter += 1
-        
+        node.endWith = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
         node = self.root
         for char in word:
             if char not in node.children:
                 return False
             node = node.children[char]
-        
-        # Reached at the end of word
-        # return True if word is present, i.e is_end = True else False
-        return node.is_end
+        return node.endWith
         
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         node = self.root
         for char in prefix:
             if char not in node.children:
                 return False
             node = node.children[char]
         return True
-        
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
