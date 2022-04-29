1b7d2c3e1ff622478d3eaa319c3cb3ee75cad53a
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-04-29 23:29:48 +0300

    Time: 77 ms (49.91%), Space: 14.3 MB (5.72%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,19 +1,58 @@
-from heapq import heappop, heapify, heappush
+class TrieNode:
+    def __init__(self, char = ""):
+        self.children = {}
+        self.endWith = False
+class Tries:
+    def __init__(self):
+        self.dic = {}
+        self.root = TrieNode()
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new_node = TrieNode(char)
+                node.children[char] = new_node
+                node = new_node
+        node.endWith = True
+        if word in self.dic:
+            self.dic[word]-=1
+        else:
+            self.dic[word] = -1
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            else:
+                node = node.children[char]
+        return node.endWith
+    def startWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            else:
+                node = node.children[char]
+        return True
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        dic = {}
+        t = Tries()
         for word in words:
-            if word in dic:
-                dic[word]-=1
-            else:
-                dic[word]=-1
-        l = []
-        for i in dic:
-            t = (dic[i], i)
-            heappush(l, t)
-        result = []
+            t.insert(word)
+        pos = []
+        for key in t.dic:
+            tup = (t.dic[key],key)
+            heappush(pos, tup)
+        result =  []
         for _ in range(k):
-            result.append(heappop(l)[1])
+            result.append(heappop(pos)[1])
         return result
+         
+        
+        
+        
+        
         
         
