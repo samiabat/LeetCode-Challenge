f9fa38e25a91b5f69533b5b800cf103683cd8366
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-05-12 10:12:07 +0300

    Time: 48 ms (66.62%), Space: 14.1 MB (59.38%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        dic = Counter(nums)
+        def backTrak(res = []):
+            if len(res)==len(nums):
+                ans.append(res)
+            for key in dic:
+                if dic[key]:
+                    dic[key]-=1
+                    backTrak(res + [key])
+                    dic[key]+=1
+            
+        backTrak()
+        return ans
+                
