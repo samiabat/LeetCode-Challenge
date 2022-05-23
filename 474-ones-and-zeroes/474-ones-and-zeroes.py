0439b7f362254021f813dfe628456a56f48fc224
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-05-23 04:11:45 +0300

    Time: 198 ms (100.00%), Space: 14.6 MB (45.52%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        counter = Counter([(s.count('0'), s.count('1')) for s in strs])
+        d = defaultdict(lambda: -1)
+        
+        def dfs(m, n, count):
+            if d[(m, n)] >= count: return d[(m, n)]
+            max_count = count
+            d[(m, n)] = count
+            
+            for k, v in counter.items():
+                if v <= 0: continue
+                zeros, ones = k[0], k[1]
+                if zeros <= m and ones <= n:
+                    counter[k] -= 1
+                    max_count = max(max_count, dfs(m - zeros, n - ones, count + 1))
+                    counter[k] += 1
+            return max_count        
+        result = dfs(m, n, 0)
+        return result
