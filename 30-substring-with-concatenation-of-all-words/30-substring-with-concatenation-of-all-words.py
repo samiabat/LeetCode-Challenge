db41779168ec8ff95f84d2540ec3b3cb23d5ba3f
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2022-07-10 12:17:22 +0300

    Time: 739 ms (48.36%), Space: 14.3 MB (14.67%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -3,32 +3,27 @@ class Solution:
         n = len(s)
         k = len(words)
         word_length = len(words[0])
-        substring_size = word_length * k
         word_count = collections.Counter(words)
+        sub_str_length = word_length*k
         
-        def check(i):
-            # Copy the original dictionary to use for this index
+        
+        
+        ans = []
+        for i in range(n-sub_str_length+1):
+            word_used = 0
             remaining = word_count.copy()
-            words_used = 0
-            
-            # Each iteration will check for a match in words
-            for j in range(i, i + substring_size, word_length):
-                sub = s[j : j + word_length]
-                if remaining[sub] > 0:
-                    remaining[sub] -= 1
-                    words_used += 1
+            for j in range(i, i+sub_str_length, word_length):
+                word = s[j: word_length+j]
+                if remaining[word]>0:
+                    remaining[word]-=1
+                    word_used +=1
                 else:
                     break
+            if word_used == k:
+                ans.append(i)
+        return ans
+                
             
-            # Valid if we used all the words
-            return words_used == k
-        
-        answer = []
-        for i in range(n - substring_size + 1):
-            if check(i):
-                answer.append(i)
-
-        return answer
                 
             
                 
