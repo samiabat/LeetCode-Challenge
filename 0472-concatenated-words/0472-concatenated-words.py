ea2fa6fec3d174f1f765c6326a982089d81353b1
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-01-29 16:50:48 -0500

    Time: 1111 ms (26.48%), Space: 16.6 MB (88.62%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -1,45 +1,16 @@
-class Trie:
-    def __init__(self):
-        self.child = {}
-        self.end = False
-        
-    
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        # insert
-        
-        root = Trie()
-        
-        
-        def insert(word):
-            node = root
-            
-            for char in word:
-                if char not in node.child:
-                    node.child[char] = Trie()
-                node = node.child[char]
-            node.end = True
-        @cache
-        def find(idx, word, node):
-            if idx >= len(word): 
-                if node.end: return True
-                return False
-            if word[idx] not in node.child and word[idx] not in root.child:
-                return False
-            
-            elif word[idx] not in node.child and node.end:
-                return find(idx+1, word, root.child[word[idx]])
-            elif word[idx] not in node.child: return False
-            one = False
-            if  word[idx] in root.child and node.end: one = find(idx+1, word, root.child[word[idx]])
-            return ( one or find(idx+1, word, node.child[word[idx]]))
-        words.sort(key=lambda x: len(x))
+        st = set(words)
         ans = []
         for word in words:
-            if find(0, word, root): ans.append(word)
-            insert(word)
+            dp = [0] * (len(word)  + 1)
+            dp[-1] = 1
+            for i in range(len(word)-1, -1, -1):
+                for j in range(i, len(word)):
+                    if word[i:j+1] in st and dp[j+1]:
+                        dp[i] = max(dp[i], 1 + dp[j+1])
+            if dp[0] > 2: ans.append(word)
         return ans
+            
         
-                
-                
-                
+        
