fb30241701d70b159af59fd34b60209edf009bbf
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-01-30 16:07:35 -0500

    Time: 781 ms (26.98%), Space: 32.5 MB (23.36%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
@@ -1,12 +1,12 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        left, right, loc, ans = 0, sum(nums), Counter(), inf
+        left, right, loc, ans = 0, sum(nums), Counter(), len(nums)
         for i in range(len(nums)):
             left += nums[i]
             right -= nums[i]
             loc[left % p] = i
             if not left % p: loc[p] = i
-            if i < len(nums)-1 and right % p == 0: ans = min(ans, i+1)
+            if right % p == 0: ans = min(ans, i+1)
             if p - right % p in loc: ans = min(ans, i - loc[p - right % p])
-        return ans if ans != inf else -1
+        return ans if ans < len(nums) else -1
             
