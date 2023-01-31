85f9dd2f014ff4137b0abf5e217a5119ae71bb5b
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-01-31 16:54:41 -0500

    Time: 34 ms (75.25%), Space: 13.8 MB (61.37%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -4,15 +4,11 @@ class Solution:
         cnt_white = 0
         ans = len(blocks)
         while right < len(blocks):
-            if right == k:
-                ans = min(ans, cnt_white)
             if right >= k:
-                cnt_white -= int(blocks[left] == 'W')
-                cnt_white += int(blocks[right] == 'W')
                 ans = min(ans, cnt_white)
+                cnt_white -= int(blocks[left] == 'W')
                 left += 1
-            else:
-                cnt_white += int(blocks[right] == 'W')
+            cnt_white += int(blocks[right] == 'W')
             right += 1
         return min(ans, cnt_white)
                 
