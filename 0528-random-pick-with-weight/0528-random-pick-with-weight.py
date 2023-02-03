836cab3ffc31fdea3127a3f5895eafad67dd6881
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-02-03 14:00:31 -0500

    Time: 168 ms (97.39%), Space: 18.4 MB (91.06%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,12 @@
+class Solution:
+    def __init__(self, w: List[int]):
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        target = self.total_sum * random.random()
+        return bisect_left(self.prefix_sums, target)
