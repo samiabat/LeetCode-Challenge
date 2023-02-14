706829277c39d2821f04d55e0cda5c3627970c67
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-02-14 00:48:33 -0500

    Time: 120 ms (30.39%), Space: 13.9 MB (53.27%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,13 +1,13 @@
 class Solution:
+    def solve(self,i,val,n):
+        if i >= 0:
+            if i == 0:return 1
+            else: return 3 * self.solve(i-1,val,n)
     def isPowerOfThree(self, n: int) -> bool:
-        def pow_three(exponent):
-            if pow(3, exponent) == n:
-                return True
-            if exponent > 31:
-                return False
-            return pow_three(exponent+1)
-        return pow_three(-32)
-            
+        if n <= 0: return False
+        
+        val = ceil(log(n)/log(3))
+        return n == self.solve(val,val,n)
             
         
         
