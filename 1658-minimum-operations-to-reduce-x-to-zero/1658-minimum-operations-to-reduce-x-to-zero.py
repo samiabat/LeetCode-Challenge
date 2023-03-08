1caa6eca2052b95951222d26489600232e71d1f5
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-03-08 10:17:39 -0500

    Time: 1302 ms (32.57%), Space: 35.8 MB (12.47%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,15 +1,20 @@
 class Solution:
-    def minOperations(self, nums: List[int], x: int) -> int:
-        left, right, ans, loc = 0, sum(nums), inf, Counter()
-        loc[0] = -1
-        for i in range(len(nums)):
-            left += nums[i]
-            loc[left] = i
-            right -= nums[i]
-            if x - right in loc:
-                ans = min(ans, len(nums) - i + loc[x - right])
-        return ans if ans != inf else -1
-                
+    def minOperations(self, arr: List[int], x: int) -> int:
+        position = {0:-1}
+        cur_prefix = 0
+        tot = sum(arr)
+        to_be_removed = tot - x
+        best_ans = float('inf')
+        for i in range(len(arr)):
+            cur_prefix += arr[i]
+            removed = cur_prefix -  to_be_removed
+            position[cur_prefix] = i
+            if removed in position:
+                cur_ans = position[removed] + 1 + (len(arr) - i - 1)
+                best_ans = min(best_ans, cur_ans)
+        if best_ans == inf:
+            return -1
+        return best_ans
             
             
         
