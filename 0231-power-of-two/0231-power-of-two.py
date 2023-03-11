b8ccf6ad86b8c7cebbbd0df9811d21b4d61d22a4
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-03-11 03:04:31 -0500

    Time: 27 ms (93.98%), Space: 13.8 MB (42.27%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,12 +1,7 @@
+import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<0: return False
-        def check_pow(exponet):
-            if exponet > 32: return False
-            if pow(2, exponet) == n:
-                return True
-            return check_pow(exponet + 1)
-        return check_pow(0)
+        return n and not (n & n-1);
         
         
                 
