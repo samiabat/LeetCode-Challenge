7353166c7b9d0e36d9c4b07ed62832ff1a37aaa2
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-03-14 13:43:54 -0400

    Time: 32 ms (65.66%), Space: 13.8 MB (54.24%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -2,9 +2,7 @@ class Solution:
     def curstomPow(self, x, n):
         if n == 0: return 1
         half = self.curstomPow(x, n//2)
-        if n % 2:
-            return half * half * x
-        return half * half
+        return half * half * x ** (n%2)
     
     def myPow(self, x: float, n: int) -> float:
         if n >= 0: return self.curstomPow(x,n)
