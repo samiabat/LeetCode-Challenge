ed9f74dd8b5450529ef53082f302f00dca176f16
Samuel Abatneh <91486438+samiabat@users.noreply.github.com>
2023-03-22 07:46:51 -0400

    Time: 29 ms (84.62%), Space: 13.9 MB (68.48%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -2,18 +2,14 @@ class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         ans = []
         arr = [str(i) for i in range(1, n+1)]
-        m = n
         def helper(n, k, prev, idx):
-            if k == 0: 
-                ans.extend(sorted(arr))
+            if n == 1: 
+                ans.append(arr[0])
                 return
-            if n>= 1 and k <= factorial(n-1):
-                ans.append(arr[prev])
-                arr.pop(0)
-                arr.sort()
+            if k <= factorial(n-1):
+                ans.append(arr.pop(0))
                 helper(n-1, k, 0, 1)
             else:
-                if idx >= len(arr): return 
                 arr[idx], arr[prev] = arr[prev], arr[idx]
                 helper(n, k-factorial(n-1), prev, idx+1)
         helper(n, k, 0, 1)
